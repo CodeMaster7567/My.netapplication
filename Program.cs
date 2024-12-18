4a1d8bd83c80e36584a7cc6a78eb0f71adb6ea9e
CodeMaster7567 <Jonasvangenechten35@gmail.com>
2024-12-18 12:47:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> tasks = new List<string>();
+        string input;
+
+        Console.WriteLine("Task Manager - To-Do List");
+        Console.WriteLine("Enter tasks, type 'exit' to finish:");
+
+        while (true)
+        {
+            Console.Write("Task: ");
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+                break;
+
+            tasks.Add(input);
+        }
+
+        Console.WriteLine("\nYour Tasks:");
+        foreach (var task in tasks)
+        {
+            Console.WriteLine("- " + task);
+        }
+    }
+}
